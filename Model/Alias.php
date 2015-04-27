45c1ab7774658c4e88cb0850c7c71d250ccf7a8d
Sergey Chernecov <sergey.chernecov@intexsys.lv>
2015-04-27 10:35:04 +0300

    Improvements part 1

---
+++ Model/Alias.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of the Ecentria software.
+ *
+ * (c) 2014, OpticsPlanet, Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Ecentria\Libraries\CoreRestBundle\Model;
+
+/**
+ * Events
+ *
+ * @author Sergey Chernecov <sergey.chernecov@intexsys.lv>
+ */
+final class Alias
+{
+    /**
+     * Alias for any request attribute data
+     */
+    const DATA = 'alias.data';
+}
