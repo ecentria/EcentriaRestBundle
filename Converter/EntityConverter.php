73bc4e5510b90a9979d277ef78354ef5c24fba3d
Ryan Wood <ryan.wood@opticsplanet.com>
2015-06-14 21:18:47 -0500

    Entity converter update to allow creation of entities

--- Converter/EntityConverter.php
+++ Converter/EntityConverter.php
@@ -16,6 +16,8 @@ use Ecentria\Libraries\EcentriaRestBundle\Model\CRUD\CrudEntityInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\DoctrineParamConverter as BaseDoctrineParamConverter;
 use Symfony\Component\HttpFoundation\Request;
+use Ecentria\Libraries\EcentriaRestBundle\Services\CRUD\CrudTransformer;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * Modified DoctrineParamConverter.
@@ -25,6 +27,24 @@ use Symfony\Component\HttpFoundation\Request;
 class EntityConverter extends BaseDoctrineParamConverter
 {
     /**
+     * CRUD Transformer
+     *
+     * @var CrudTransformer
+     */
+    private $crudTransformer;
+
+    /**
+     * Constructor
+     *
+     * @param CrudTransformer $crudTransformer crudTransformer
+     */
+    public function __construct(CrudTransformer $crudTransformer, ManagerRegistry $registry = null)
+    {
+        $this->crudTransformer = $crudTransformer;
+        $this->registry = $registry;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function apply(Request $request, ParamConverter $configuration)
@@ -32,20 +52,15 @@ class EntityConverter extends BaseDoctrineParamConverter
         $name    = $configuration->getName();
         $class   = $configuration->getClass();
         $options = $this->getOptions($configuration);
+        $create  = !empty($options['mode']) && $options['mode'] == 'create';
 
         if (null === $request->attributes->get($name, false)) {
             $configuration->setIsOptional(true);
         }
 
-        // find by identifier?
-        if (null === $object = $this->find($class, $request, $options, $name)) {
-            // find by criteria
-            if (null === $object = $this->findOneBy($class, $request, $options)) {
-                $object = new $class();
-                if ($object instanceof CrudEntityInterface) {
-                    $object->setId($request->attributes->get('id'));
-                }
-            }
+        $object = $create ? null : $this->findObject($class, $request, $options, $name);
+        if (is_null($object)) {
+            $object = $this->createNewObject($class, $request, $create, $options);
         }
 
         $request->attributes->set($name, $object);
@@ -57,5 +72,53 @@ class EntityConverter extends BaseDoctrineParamConverter
         $request->attributes->set(Alias::DATA, $name);
 
         return true;
+    }
+
+    /**
+     * @param $class
+     * @param $request
+     * @param $options
+     * @param $name
+     * @return bool|mixed
+     */
+    private function findObject($class, $request, $options, $name)
+    {
+        $object = null;
+        // find by identifier?
+        if (null === $object = $this->find($class, $request, $options, $name)) {
+            // find by criteria
+            $object = $this->findOneBy($class, $request, $options);
+        }
+        return $object;
+    }
+
+    /**
+     * Create New Object
+     *
+     * @param string  $class
+     * @param Request $request
+     * @param bool    $create
+     * @return CrudEntityInterface|mixed
+     */
+    private function createNewObject($class, Request $request, $create, $options)
+    {
+        $data = $create ? json_decode($request->getContent(), true) : array();
+        $object = $this->crudTransformer->arrayToObject($data, $class);
+        if (!$create && $object instanceof CrudEntityInterface) {
+            $object->setId($request->attributes->get('id'));
+        }
+        if (isset($options['references'])) {
+            $references = !is_array(current($options['references'])) ? array($options['references']) : $options['references'];
+            foreach ($references as $reference) {
+                $entity = $this->findObject(
+                    $reference['class'], $request, array_merge($reference, $options), $reference['name']
+                );
+                $setter = $this->crudTransformer->getPropertySetter($reference['name']);
+                if (method_exists($object, $setter) && is_object($entity)) {
+                    $object->$setter($entity);
+                }
+            }
+        }
+        return $object;
     }
 }
