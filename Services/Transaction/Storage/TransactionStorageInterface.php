cec0ebc8060e389e920823a69cd73364bf6a6626
Artem Petrov <artem.petrov@OpticsPlanet.com>
2016-07-05 19:24:33 -0500

    updated comments

--- Services/Transaction/Storage/TransactionStorageInterface.php
+++ Services/Transaction/Storage/TransactionStorageInterface.php
@@ -1,9 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: artem.petrov
- * Date: 7/1/16
- * Time: 2:24 PM
+/*
+ * This file is part of the ecentria group, inc. software.
+ *
+ * (c) 2016, ecentria group, inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Ecentria\Libraries\EcentriaRestBundle\Services\Transaction\Storage;
@@ -13,7 +15,19 @@ use Ecentria\Libraries\EcentriaRestBundle\Model\Transaction;
 
 interface TransactionStorageInterface {
 
+    /**
+     * Read Transaction Model from Transaction Storage
+     *
+     * @param $id
+     * @return Transaction
+     */
     public function read($id);
 
+    /**
+     * Write Transaction Model to Transaction Storage
+     *
+     * @param Transaction $transaction
+     * @return void
+     */
     public function write(Transaction $transaction);
 } 
